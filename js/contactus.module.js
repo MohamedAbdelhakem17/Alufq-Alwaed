6c209280173330576c2a0c945dd4f26ac5f286c2
Mohamed Abdelhakem <mohamed.abdelhakem3200@gmail.com>
2024-03-28 00:37:40 +0200

    Add Loader To Send Data Btn

--- js/contactus.module.js
+++ js/contactus.module.js
@@ -10,6 +10,7 @@ const emailError = document.getElementById("email-error")
 const subjectError = document.getElementById("subject-error")
 const massageError = document.getElementById("massage-error")
 let timeoutId;
+let isLoad = false;
 
 let emailData = {
     name: "",
@@ -28,39 +29,58 @@ const collectData = () => {
 
     return emailData
 };
+
 const showError = (element, massage) => {
     element.classList.remove("d-none")
     element.innerHTML = massage
 }
+
+function showLoader() {
+    submitButton.innerHTML = `<i class="fa-solid fa-spinner fa-spin"></i>`
+    submitButton.disabled = true;
+}
+
+function hideLoader() {
+    submitButton.innerHTML = "Send Massage"
+    submitButton.disabled = false;
+}
+
 const sendData = async (emailData) => {
-    const API_URL = "https://alufqalwaed.com/send-email.php"
-    const data = await fetch(API_URL, {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(emailData)
-    })
-    const response = await data.json();
-    if (response.status) {
-        mainError.classList.add("text-success")
-        showError(mainError, response.message)
-        userName.value = ""
-        userEmail.value = ""
-        userMessage.value = ""
-        userSubject.value = ""
-        timeoutId = setTimeout(() => {
-            mainError.classList.add("d-none")
-            mainError.classList.remove("text-success")
-            clearTimeout(timeoutId)
-        }, 1500)
-    } else {
-        showError(mainError, response.message)
-        const { errors } = response
-        errors.name && showError(nameError, errors.name)
-        errors.email && showError(emailError, errors.email)
-        errors.message && showError(massageError, errors.message)
-        errors.subject && showError(subjectError, errors.subject)
+    try {
+        showLoader()
+        const API_URL = "https://alufqalwaed.com/send-email.php"
+        const data = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(emailData)
+        })
+        const response = await data.json();
+        if (response.status) {
+            mainError.classList.add("text-success")
+            showError(mainError, response.message)
+            userName.value = ""
+            userEmail.value = ""
+            userMessage.value = ""
+            userSubject.value = ""
+            timeoutId = setTimeout(() => {
+                mainError.classList.add("d-none")
+                mainError.classList.remove("text-success")
+                clearTimeout(timeoutId)
+            }, 1500)
+        } else {
+            showError(mainError, response.message)
+            const { errors } = response
+            errors.name && showError(nameError, errors.name)
+            errors.email && showError(emailError, errors.email)
+            errors.message && showError(massageError, errors.message)
+            errors.subject && showError(subjectError, errors.subject)
+        }
+        hideLoader()
+    } catch {
+        showError(mainError, "Unable to connect to the internet. Please check your network connection and try again")
+        hideLoader()
     }
 }
 
